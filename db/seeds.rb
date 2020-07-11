c87b697929e256588ea4bd153a1153f688eb8dcc
Hitesh3202 <hitesh.sarangal@fourkites.com>
2020-07-11 15:56:48 +0530

    Added Raw data -- seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+10.times do |index|
+  User.create!({
+    username: Faker::Name.name,
+    password: 'password'
+  })
+end
 
 20.times do |index|
   Message.create!({
